192afc955eac81c8361f38ba9b9b30a1b8fc3589
Shrena Patel <shrena.p@hotmail.co.uk>
2024-06-07 11:31:29 +1200

    add items table

---
+++ server/db/migrations/20240606231541_items.js
@@ -0,0 +1,15 @@
+export async function up(knex) {
+  return knex.schema.createTable('items', (table) => {
+    table.increments().primary()
+    table.string('name')
+    table.string('image')
+    table.integer('days_worn')
+    table.float('cost_per_wear')
+    table.integer('category_id')
+    table.integer('user_id')
+  })
+}
+
+export async function down(knex) {
+  return knex.schema.dropTable('items')
+}

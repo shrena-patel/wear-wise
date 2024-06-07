00e9f579190bbbd5dd8baf96c6f8318b7965e3cb
Shrena Patel <shrena.p@hotmail.co.uk>
2024-06-07 12:05:22 +1200

    setup clerk auth

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,27 @@
-import { useFruits } from '../hooks/useFruits.ts'
+// import { useFruits } from '../hooks/useFruits.ts'
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from '@clerk/clerk-react'
 
 function App() {
-  const { data } = useFruits()
+  // const { data } = useFruits()
 
   return (
     <>
       <div className="app">
         <h1>Wear Wise</h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
+        {/* <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul> */}
+        <header>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </header>
       </div>
     </>
   )
